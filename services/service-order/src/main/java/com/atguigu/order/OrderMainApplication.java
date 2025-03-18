704b94afe17aa2b4106d3edbc0d1dc851cae374f
qiangi <2071894243@qq.com>
2025-03-18 08:17:27 +0800

    进行监听nacos配置中心中的配置的变化,如果配置文件发生了变化,那么就进行邮件的通知

--- services/service-order/src/main/java/com/atguigu/order/OrderMainApplication.java
+++ services/service-order/src/main/java/com/atguigu/order/OrderMainApplication.java
@@ -1,13 +1,45 @@
 package com.atguigu.order;
 
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.config.listener.Listener;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 public class OrderMainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderMainApplication.class, args);
+    }
+
+
+    /**
+     * 进行监听nacos配置中心中的配置的变化,如果配置文件发生了变化,那么就进行邮件的通知
+     * @param nacosConfigManager
+     * @return
+     */
+    @Bean
+    ApplicationRunner applicationRunner(NacosConfigManager nacosConfigManager) {
+        return args -> {
+            ConfigService configService = nacosConfigManager.getConfigService();
+            configService.addListener("service-order.properties", "DEFAULT_GROUP", new Listener() {
+                @Override
+                public Executor getExecutor() {
+                    return null;
+                }
+
+                @Override
+                public void receiveConfigInfo(String configInfo) {
+                    System.out.println("变化的配置信息:"+configInfo);
+                    System.out.println("邮件通知......");
+                }
+            });
+        };
     }
 }
