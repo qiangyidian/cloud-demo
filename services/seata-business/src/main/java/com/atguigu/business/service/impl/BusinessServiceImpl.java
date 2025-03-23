ecb6ef7ebb51b1146f2982ea8d2d98837667d1f0
qiangi <2071894243@qq.com>
2025-03-23 19:06:27 +0800

    课程完结

--- services/seata-business/src/main/java/com/atguigu/business/service/impl/BusinessServiceImpl.java
+++ services/seata-business/src/main/java/com/atguigu/business/service/impl/BusinessServiceImpl.java
@@ -1,15 +1,30 @@
 package com.atguigu.business.service.impl;
 
+import com.atguigu.business.feign.OrderFeignClient;
+import com.atguigu.business.feign.StorageFeignClient;
 import com.atguigu.business.service.BusinessService;
+import org.apache.seata.spring.annotation.GlobalTransactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class BusinessServiceImpl implements BusinessService {
+
+    @Autowired
+    StorageFeignClient storageFeignClient;
+
+    @Autowired
+    OrderFeignClient orderFeignClient;
+
+    @GlobalTransactional
     @Override
     public void purchase(String userId, String commodityCode, int orderCount) {
-        //TODO 1. 扣减库存
 
+
+        //TODO 1. 扣减库存
+        storageFeignClient.deduct(commodityCode, orderCount);
         //TODO 2. 创建订单
+        orderFeignClient.create(userId, commodityCode, orderCount);
     }
 }
